59d8b0a011f898ab899bb151d8aa8b03aec969a3
AN321657 <anchitraj@gmail.com>
2017-08-05 21:50:50 +0530

    Initial Commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name horukuApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the horukuApp
+ */
+angular.module('horukuApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name horukuApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the horukuApp
+ */
+angular.module('horukuApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
